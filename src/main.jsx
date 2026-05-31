51e1fbefa6e64606b43e96fd7cb9fed985dc6a5e
AllemSamyel-03 <allemsamyel13@gmail.com>
2026-06-01 01:03:17 +0530

    Updated browseRouter to HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOMClient from "react-dom/client";
-import { BrowserRouter } from "react-router";
+import { HashRouter } from "react-router";
 import App from "./App";
 import "./index.css";
 
@@ -8,8 +8,8 @@ const root = ReactDOMClient.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 );
